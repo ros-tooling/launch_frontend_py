60bac497321741275516d2c4adb6b6d740b72702
Emerson Knapp <emerson.b.knapp@gmail.com>
2025-05-15 00:21:26 -0700

    Remove debug block

--- test/basic_launch.py
+++ test/basic_launch.py
@@ -4,7 +4,3 @@ def generate_launch_description():
     return launch([
         executable(cmd='echo hello launch_py world!', output='screen')
     ])
-
-if __name__ == '__main__':
-    ld = generate_launch_description()
-    print(ld)
